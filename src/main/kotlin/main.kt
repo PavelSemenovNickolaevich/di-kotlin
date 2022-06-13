df7f8d585d0b1d7dc42e7b3f39ce545ff44ab38b
Pavel Semenov <PNSemenov@sberbank.ru>
2022-06-13 20:07:16 +0300

    injection with interface

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,24 +12,42 @@ fun main(args: Array<String>) {
     }
 
     // тестируем DI
-    val testKoin = TestKoin()
-    print(testKoin.getCompName())
+    val testKoin = TestKoin() // класс, в котором внедряются объекты
+    testKoin.comp1.on() // пробуем вызвать метод у внедренного объекта
 
 }
 
+// этот интерфейс будет реализовавать объект
+// вместо конкретного типа, при внедрении будет указываться интефейс
+interface Technica{
+    fun on()
+    fun off()
+}
+
 // его будем внедрять в коде где необходимо
-data class Computer(val name: String)
+class Computer(val name: String): Technica {
+
+    // функции-реализации интерфейса Technica
+
+    override fun on() {
+        println("$name + on")
+    }
+
+    override fun off() {
+        println("$name + off")
+    }
+
+}
 
 // DI контейнер, в котором указываем какие объекты будут возвращаться и их область действия
 val injModule = module {
-    single {Computer("test")} // single - область действия объекта (scope)
+    // "as" указывать обязательно, иначе DI не сработает (даже если IDEA рекомендует удалить as)
+    single {Computer("IBM") as Technica} // single - область действия объекта (scope)
+
 }
 
 // с помощью KoinComponent мы теперь можем внедрять объекты в коде
 class TestKoin: KoinComponent {
-    val comp1 by inject<Computer>()
+    val comp1 by inject<Technica>() // само внедрение объекта по типу интерфейса
 
-    fun getCompName(): String{
-        return comp1.name
-    }
 }
